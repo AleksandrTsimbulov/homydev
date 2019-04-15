9767c6758babd63b72dfd82ac5f57ba0f06966da
AleksandrTsimbulov <staile@yandex.ru>
2019-04-15 22:13:00 +0500

    jinja updated

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'isdjfoadfnaogi84903gldjg9eu0gljs9e3jpj3p9gldjgo93u04telkkflgjoeut9'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://homychat:boardgames@localhost/homychat_db3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
